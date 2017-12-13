6cabb6b17c02b65e6fe0f96df12834dab25dc4fd
Juma Baraka <30748242+barack98@users.noreply.github.com>
2017-12-13 22:20:30 +0300

    Update settings.py

--- MyTruckCo/settings.py
+++ MyTruckCo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wsf)r-5)+=c40pvtf6&6ls@v42-ps_7fv#5x&9!n_b)_+!5%c)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
